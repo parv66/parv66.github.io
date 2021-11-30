c836c60dd1be386cbd1b42218838bc42b91a2275
parv66 <82944668+parv66@users.noreply.github.com>
2021-11-30 16:31:14 +0530

    Update clickingsound.js

--- clickingsound.js
+++ clickingsound.js
@@ -6,7 +6,9 @@ var one = 0;
 function error666() { 
   one++
   if (one == 3) {
-    console.log('yes');
+    var audio = document.getElementById("error666");
+    audio.play();
+    document.getElementByID('html').style.animation="error 4s linear infinite";
     one = 0
   }
 }
