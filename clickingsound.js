984c7355c847d9fdcc2ac78b24df7d181994a08f
parv66 <82944668+parv66@users.noreply.github.com>
2021-12-01 22:54:51 +0530

    Update clickingsound.js

--- clickingsound.js
+++ clickingsound.js
@@ -2,14 +2,3 @@ function play() {
         var audio = document.getElementById("buttonclick");
         audio.play();
       }
-var one = 0;
-function err() { 
-   one++
-   if (one == 3) {
-      var element = document.getElementById("errl");
-      element.classList.add("errl");
-            var audio = document.getElementById("errors");
-            audio.play();
-            audio.volume = 0.2;
-  }
-}
