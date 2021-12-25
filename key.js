364180407b0bcfdc50842057c2bfd789a47c38b7
PARV66 <82944668+parv66@users.noreply.github.com>
2021-12-25 23:30:27 +0530

    Create key.js

---
+++ key.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const options = {
+        eventType: 'keydown',
+        keystrokeDelay: 400
+    };
+
+    keyMapper([updateBackground, updateUI], options);
+});
+
+function keyMapper(callbackList, options) {
+    const delay = hasProperty('keystrokeDelay', options) && options.keystrokeDelay >= 300 && options.keystrokeDelay;
+    const keystrokeDelay = delay || 1000;
+    const eventType = hasProperty('eventType', options) && options.eventType || 'keydown';
+
+    let state = {
+        buffer: [],
+        lastKeyTime: Date.now()
+    };
+
+    document.addEventListener(eventType, event => {
+        const key = event.key;
+        const currentTime = Date.now();
+        let buffer = [];
+
+        if (currentTime - state.lastKeyTime > keystrokeDelay) {
+            buffer = [key];
+        } else {
+            buffer = [...state.buffer, key];
+        }
+
+        state = {buffer: buffer, lastKeyTime: currentTime};
+
+        callbackList.forEach(callback => callback(buffer));
+    });
+
+    function hasProperty(property, object) {
+        return object && object.hasOwnProperty(property);
+    }
+}
+
+function updateBackground(keySequence) {
+    const validKeys = keySequence.every(key => !isNaN(parseInt(key)) || key.toLowerCase() !== key.toUpperCase());
+    if (!validKeys) return;
+    const container = document.querySelector('#background');
+    container.style.backgroundImage = `url(images/${keySequence.join('')}.jpg)`;
+}
+
+function updateUI(keySequence) {
+    const userInput = keySequence.join('').toLowerCase();
+    const keySequences = {
+        'idfa': 'All Weapons + Ammo',
+        'idkfa': 'All Weapons + Ammo + Keys',
+        'idbeholds': 'Beserk Pack',
+        'idclev31': 'Bonus Level'
+    };
+    if (keySequences === 'test666') {
+      var element = document.getElementById("errl");
+      element.classList.add("errl");
+    } else {
+      const keySequences = {
+       'test666': 'test succeds'
+      }
+    }
+    const userInputDisplay = document.querySelector('#user_input');
+    userInputDisplay.textContent = userInput;
+
+    const cheatMessage = document.querySelector('#cheat_message');
+    cheatMessage.textContent = keySequences[userInput] || 'Nothing';
+}
