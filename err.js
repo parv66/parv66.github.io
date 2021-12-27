cb1799658547369227dde425dff68a4850de1448
PARV66 <82944668+parv66@users.noreply.github.com>
2021-12-27 22:18:44 +0530

    Update err.js

--- err.js
+++ err.js
@@ -1,18 +1,8 @@
-document.addEventListener("keydown", function(event) {
-  event.preventDefault();
-  switch (event.which) {
-    case 112:
-     err();
-  }
-})
-var one = 0;
+
 function err() { 
-   one++
-   if (one == 3) {
       var element = document.getElementById("errl");
       element.classList.add("errl");
             var audio = document.getElementById("errors");
             audio.play();
             audio.volume = 0.2;
-  }
 }
