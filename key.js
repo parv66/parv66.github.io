601bcdc61e73c5b508995d3b0a9a267a53e5efc7
PARV66 <82944668+parv66@users.noreply.github.com>
2021-12-27 20:30:23 +0530

    Update key.js

--- key.js
+++ key.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         keystrokeDelay: 400
     };
 
-    keyMapper([updateBackground, updateUI], options);
+    keyMapper([updateUI], options);
 });
 
 function keyMapper(callbackList, options) {
@@ -39,30 +39,10 @@ function keyMapper(callbackList, options) {
         return object && object.hasOwnProperty(property);
     }
 }
-
-function updateBackground(keySequence) {
-    const validKeys = keySequence.every(key => !isNaN(parseInt(key)) || key.toLowerCase() !== key.toUpperCase());
-    if (!validKeys) return;
-}
-
 function updateUI(keySequence) {
     const userInput = keySequence.join('').toLowerCase();
-    const keySequences = {
-        'idfa': 'All Weapons + Ammo',
-        'idkfa': 'All Weapons + Ammo + Keys',
-        'idbeholds': 'Beserk Pack',
-        'idclev31': 'Bonus Level'
-    };
-    if (userInput === 'test1') {
+    if (userInput === 'l') {
       var element = document.getElementById("errl");
       element.classList.add("errl");
-            var audio = document.getElementById("errors");
-            audio.play();
-            audio.volume = 0.2;
   }
-    const userInputDisplay = document.querySelector('#user_input');
-    userInputDisplay.textContent = userInput;
-
-    const cheatMessage = document.querySelector('#cheat_message');
-    cheatMessage.textContent = keySequences[userInput] || 'Nothing';
 }
