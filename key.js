ecb040cbeff868b151238a5866d2f98e8a7fd24a
PARV66 <82944668+parv66@users.noreply.github.com>
2021-12-27 21:22:26 +0530

    Update key.js

--- key.js
+++ key.js
@@ -44,5 +44,8 @@ function updateUI(keySequence) {
     if (userInput === 'l') {
       var element = document.getElementById("errl");
       element.classList.add("errl");
+            var audio = document.getElementById("errors");
+            audio.play();
+            audio.volume = 0.2;
   }
 }
