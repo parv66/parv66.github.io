1b803e341aea9c67f06db1c0cbb2bf6cf0d6f273
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-22 18:12:02 +0530

    Update trade.js

--- trade/trade.js
+++ trade/trade.js
@@ -1,32 +1,29 @@
-$( document ).ready(function() {
-
 //copperplusplus trade//
-  $("#toption0").on("click", function() {
-  $(".curr").first().appendTo(".tbox1");
-  if($('#38').is(':empty')){
-  $('#copperplusplus').clone().appendTo('.tbox2');
-  }
-  else{
-  $('.tbox2').empty();
-  $('#copperplusplus').clone().appendTo('.tbox2');
-  }
-  })
-  //papple trade
-  $("#toption1").on("click", function() {
-  $(".curr").first().appendTo(".tbox1");
-  if($('#38').is(':empty')){
-  $('#papple').clone().appendTo('.tbox2');
-  }
-  else{
-  $('.tbox2').empty();
-  $('#papple').clone().appendTo('.tbox2');
-  }
-  })
-  
-  
-  
-  //clear trade slots when currency removed//
-  if ($('#38').is(':empty')) { 
+$("#toption0").on("click", function() {
+    $(".curr").first().appendTo(".tbox1");
+    if ($('#38').is(':empty')) {
+        $('#copperplusplus').clone().appendTo('.tbox2');
+    };
+    else {
+        $('.tbox2').empty();
+        $('#copperplusplus').clone().appendTo('.tbox2');
+    }
+})
+//papple trade
+$("#toption1").on("click", function() {
+    $(".curr").first().appendTo(".tbox1");
+    if ($('#38').is(':empty')) {
+        $('#papple').clone().appendTo('.tbox2');
+    };
+    else {
+        $('.tbox2').empty();
+        $('#papple').clone().appendTo('.tbox2');
+    }
+})
+
+
+
+//clear trade slots when currency removed//
+if ($('#38').is(':empty')) {
     $('.tbox2').empty();
-} 
-});
+};
