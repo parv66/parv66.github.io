b460f1b0a0bb533d88cfb4163ba511d5586ee6b8
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-22 17:36:30 +0530

    Update trade.js

--- trade/trade.js
+++ trade/trade.js
@@ -3,24 +3,22 @@ $( document ).ready(function() {
   if ( $('#38').is(':empty')){
   $(".tbox2").empty();
   }
-  else{
-  gofkyourself(idiot);
-  }
 //the click to move currency part//
   $("#toption0").on("click", function() {
   $(".curr").first().appendTo(".tbox1");
   if($('#38').is(':empty')){
   $('#copperplusplus').clone().appendTo('.tbox2');
-  }})
+  }
   else{
   $('.tbox2').empty();
   $('#copperplusplus').clone().appendTo('.tbox2');
   }
+  })
   
   $("#toption1").on("click", function() {
   $(".curr").first().appendTo(".tbox1");
   $('#papple').clone().appendTo('.tbox2');
-})
+  })
   
   $("#toption2").on("click", function() {
   $(".curr").first().appendTo(".tbox1");})
