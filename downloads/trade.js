1a8892cb0ac8e1d46091879c46ec8c0f17e4fd37
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-22 23:28:27 +0530

    Update trade.js

--- downloads/trade.js
+++ downloads/trade.js
@@ -8,6 +8,12 @@ $("#toption0").on("click", function() {
         $('.tbox2').empty();
         $('#copperplusplus').clone().appendTo('.tbox2');
     }
+    //button giv class part(giving AND removing classes accorinding to the trade)//
+    $('.tbtn').removeClass('addon1');
+    $('.tbtn').removeClass('addon2');
+    $('.tbtn').removeClass('addon3');
+    $('.tbtn').removeClass('addon4');
+    $('.tbtn').addClass('addon0');
 })
 //papple trade
 $("#toption1").on("click", function() {
@@ -19,6 +25,28 @@ $("#toption1").on("click", function() {
         $('.tbox2').empty();
         $('#papple').clone().appendTo('.tbox2');
     }
+    //button giv class part(giving AND removing classes accorinding to the trade)//
+    $('.tbtn').removeClass('addon0');
+    $('.tbtn').removeClass('addon2');
+    $('.tbtn').removeClass('addon3');
+    $('.tbtn').removeClass('addon4');
+    $('.tbtn').addClass('addon1');
 })
+//making trade button actually trade/download//
+const tbtn = document.querySelectorAll('.tbtn');
+  tbtn.addEventListener('click', moveItem);
+  tbtn.addEventListener('contextmenu', moveItem);
+  tbtn.setAttribute('draggable', false);
+//this thing works!!!!//
+if ( $('#38').children().length > 0 ) {
+      $("html").css("background-color", "yellow");
+}
+//end
 //clear trade slots when currency removed//
 //(https://parv66.tk/gui/itemdrag.js{52:5})//
+//end//
+//lists =>
+//copperplusplus: addon0
+//placeable apples: addon1
+//sweetsplusplus: addon2
+//lists.end();
