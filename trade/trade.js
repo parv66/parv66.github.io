ffcf8dc8c90352118a4fe1213b92640a99c76b33
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-22 18:26:55 +0530

    Update trade.js

--- trade/trade.js
+++ trade/trade.js
@@ -3,7 +3,7 @@ $("#toption0").on("click", function() {
     $(".curr").first().appendTo(".tbox1");
     if ($('#38').is(':empty')) {
         $('#copperplusplus').clone().appendTo('.tbox2');
-    };
+    }
     else {
         $('.tbox2').empty();
         $('#copperplusplus').clone().appendTo('.tbox2');
@@ -14,7 +14,7 @@ $("#toption1").on("click", function() {
     $(".curr").first().appendTo(".tbox1");
     if ($('#38').is(':empty')) {
         $('#papple').clone().appendTo('.tbox2');
-    };
+    }
     else {
         $('.tbox2').empty();
         $('#papple').clone().appendTo('.tbox2');
