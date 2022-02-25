69def5c575b408ef9a174129bb8de521cb0faf36
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-25 14:21:35 +0530

    Update mcbtnclick.js

--- gui/mcbtnclick.js
+++ gui/mcbtnclick.js
@@ -4,13 +4,9 @@ var ado = document.createElement('audio');
   document.body.appendChild(ado);
   //on click function//
   const mcbtn = document.querySelectorAll('.mcbtn')
-  mcbtn.forEach(listen);
-    
-  function listen(){
-    this.addEventListener("click", playsnd)
-    function playsnd(){
-    let sound = document.querySelector('#btnSnd');
-    sound.volume = 0.6;
-    sound.play();
-    }
+  mcbtn.forEach.addEventListener("click", playsnd);
+  function playsnd(){
+  let sound = document.querySelector('#btnSnd');
+  sound.volume = 0.6;
+  sound.play();
   }
