64c31870993fc464dfb7bdae4fd08b454f6a73f4
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-25 13:59:17 +0530

    Update mcbtnclick.js

--- gui/mcbtnclick.js
+++ gui/mcbtnclick.js
@@ -1,17 +1,16 @@
-$( document ).ready(function() {
-    //create soudns element
-  crtSndEle();
+var ado = document.createElement('audio');
+  ado.src = 'https://parv66.tk/random.click.ogg'
+  ado.id = 'btnSnd';
+  document.body.appendChild(ado);
   //on click function//
-  $(".mcbtn").click(function(){
-    let sound = $('#mcBtnClicked');
+  const mcbtn = document.querySelectorAll('.mcbtn')
+  mcbtn.forEach(listen);
+    
+  function listen(){
+    this.addEventListener("click", playsnd)
+    function playsnd(){
+    let sound = document.querySelector('#btnSnd');
     sound.volume = 0.6;
     sound.play();
-  });
-  //create element function//
-  function crtSndEle() {
-    var ado = document.createElement('audio');
-    ado.src = 'https://parv66.tk/random.click.ogg'
-    ado.id = 'mcBtnClicked';
-    document.body.onload = appendChild(ado);
+    }
   }
-});
