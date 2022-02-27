68be5658840bd047f3698c67ac8f8dab8ba8acda
PARV66 <82944668+parv66@users.noreply.github.com>
2022-02-27 20:47:12 +0530

    Update mcbtnclick.js

--- gui/mcbtnclick.js
+++ gui/mcbtnclick.js
@@ -10,7 +10,7 @@ window.onload=function(){
 	console.log("button click registered")
 	let sound = document.querySelector('#btnsnd');
 	sound.volume = 0.4;
-	sound.currentTime = 0;
+	sound.currentTime = 0.02;
 	sound.play();
 	console.log("playeed soundd")
 });
