6941137a08082e6cee4daa605cc924370b178783
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-07 23:36:51 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
-
-$( '#tabbtnone' ).on( 'click', function () {
-  console.log('clicked one');
-  $('#tabone').toggle();
   $('#tabtwo').toggle();
   $('#tabthree').toggle();
   $('#tabfour').toggle();
-});
-$( '#tabbtntwo' ).on( 'click', function () {
-  console.log('clicked 2');
-  $('#tabone').toggle();
-  $('#tabtwo').toggle();
-  $('#tabthree').toggle();
-  $('#tabfour').toggle();
-});
-$( '#tabbtnthree' ).on( 'click', function () {
-  console.log('clicked 3');
-  $('#tabone').toggle();
-  $('#tabtwo').toggle();
-  $('#tabthree').toggle();
-  $('#tabfour').toggle();
-});
-
-$( '#tabbtnfour' ).on( 'click', function () {
-  console.log('clicked 4')
-  $('#tabone').toggle();
-  $('#tabtwo').toggle();
-  $('#tabthree').toggle();
-  $('#tabfour').toggle();
-});
+  $( '#tabbtnone' ).on( 'click', function () {
+    console.log('clicked one');
+    $('#tabone').toggle();
+    $('#tabtwo').toggle();
+    $('#tabthree').toggle();
+    $('#tabfour').toggle();
+  });
+  $( '#tabbtntwo' ).on( 'click', function () {
+    console.log('clicked 2');
+    $('#tabone').toggle();
+    $('#tabtwo').toggle();
+    $('#tabthree').toggle();
+    $('#tabfour').toggle();
+  });
+  $( '#tabbtnthree' ).on( 'click', function () {
+    console.log('clicked 3');
+    $('#tabone').toggle();
+    $('#tabtwo').toggle();
+    $('#tabthree').toggle();
+    $('#tabfour').toggle();
+  });
+  $( '#tabbtnfour' ).on( 'click', function () {
+    console.log('clicked 4')
+    $('#tabone').toggle();
+    $('#tabtwo').toggle();
+    $('#tabthree').toggle();
+    $('#tabfour').toggle();
+  });
 });
