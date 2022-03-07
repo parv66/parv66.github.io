e86e05c0755b049fdd81902b296d5e24a0c2bc4b
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-07 23:34:36 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -2,31 +2,31 @@ $(document).ready(function () {
 
 $( '#tabbtnone' ).on( 'click', function () {
   console.log('clicked one');
-  $('#tabone').css('display', 'block');
-  $('#tabtwo').css('display', 'none');
-  $('#tabthree').css('display', 'none');
-  $('#tabfour').css('display', 'none');
+  $('#tabone').toggle();
+  $('#tabtwo').toggle();
+  $('#tabthree').toggle();
+  $('#tabfour').toggle();
 });
 $( '#tabbtntwo' ).on( 'click', function () {
   console.log('clicked 2');
-  $('#tabone').css('display', 'none');
-  $('#tabtwo').css('display', 'block');
-  $('#tabthree').css('display', 'none');
-  $('#tabfour').css('display', 'none');
+  $('#tabone').toggle();
+  $('#tabtwo').toggle();
+  $('#tabthree').toggle();
+  $('#tabfour').toggle();
 });
 $( '#tabbtnthree' ).on( 'click', function () {
   console.log('clicked 3');
-  $('#tabone').css('display', 'none');
-  $('#tabtwo').css('display', 'none');
-  $('#tabthree').css('display', 'block');
-  $('#tabfour').css('display', 'none');
+  $('#tabone').toggle();
+  $('#tabtwo').toggle();
+  $('#tabthree').toggle();
+  $('#tabfour').toggle();
 });
 
 $( '#tabbtnfour' ).on( 'click', function () {
   console.log('clicked 4')
-  $('#tabone').css('display', 'none');
-  $('#tabtwo').css('display', 'none');
-  $('#tabthree').css('display', 'none');
-  $('#tabfour').css('display', 'block');  
+  $('#tabone').toggle();
+  $('#tabtwo').toggle();
+  $('#tabthree').toggle();
+  $('#tabfour').toggle();
 });
 });
