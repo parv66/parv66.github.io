ed87f34902f04371e0bbff56b440a02fedf83f3b
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-07 19:09:43 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -1,24 +1,24 @@
 $('.tabbtn1').click(function(){
-  $('.tab1').css('z-index', '6');
-  $('.tab2').css('z-index', '-6');
-  $('.tab3').css('z-index', '-6');
-  $('.tab4').css('z-index', '-6');
+  $('.tab1').css('display', 'block');
+  $('.tab2').css('display', 'none');
+  $('.tab3').css('display', 'none');
+  $('.tab4').css('display', 'none');
 });
 $('.tabbtn2').click(function(){
-  $('.tab1').css('z-index', '-6');
-  $('.tab2').css('z-index', '6');
-  $('.tab3').css('z-index', '-6');
-  $('.tab4').css('z-index', '-6');
+  $('.tab1').css('display', 'block');
+  $('.tab2').css('display', 'none');
+  $('.tab3').css('display', 'none');
+  $('.tab4').css('display', 'none');
 });
 $('.tabbtn3').click(function(){
-  $('.tab1').css('z-index', '-6');
-  $('.tab2').css('z-index', '-6');
-  $('.tab3').css('z-index', '6');
-  $('.tab4').css('z-index', '-6');
+  $('.tab1').css('display', 'block');
+  $('.tab2').css('display', 'none');
+  $('.tab3').css('display', 'none');
+  $('.tab4').css('display', 'none');
 });
 $('.tabbtn4').click(function(){
-  $('.tab1').css('z-index', '-6');
-  $('.tab2').css('z-index', '-6');
-  $('.tab3').css('z-index', '-6');
-  $('.tab4').css('z-index', '6');
+  $('.tab1').css('display', 'block');
+  $('.tab2').css('display', 'none');
+  $('.tab3').css('display', 'none');
+  $('.tab4').css('display', 'none');
 });
