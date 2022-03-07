3b5711ab2e487ad2071e96e7dfe32cd04b58635c
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-07 20:57:29 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -1,24 +1,24 @@
-$('.tabbtnone').click(function(){
-  $('.tabone').css('display', 'block');
-  $('.tabtwo').css('display', 'none');
-  $('.tabthree').css('display', 'none');
-  $('.tabfour').css('display', 'none');
+$('#tabbtnone').click(function(){
+  $('#tabone').css('display', 'block');
+  $('#tabtwo').css('display', 'none');
+  $('#tabthree').css('display', 'none');
+  $('#tabfour').css('display', 'none');
 });
-$('.tabbtntwo').click(function(){
-  $('.tabone').css('display', 'none');
-  $('.tabtwo').css('display', 'block');
-  $('.tabthree').css('display', 'none');
-  $('.tabfour').css('display', 'none');
+$('#tabbtntwo').click(function(){
+  $('#tabone').css('display', 'none');
+  $('#tabtwo').css('display', 'block');
+  $('#tabthree').css('display', 'none');
+  $('#tabfour').css('display', 'none');
 });
-$('.tabbtnthree').click(function(){
-  $('.tabone').css('display', 'none');
-  $('.tabtwo').css('display', 'none');
-  $('.tabthree').css('display', 'block');
-  $('.tabfour').css('display', 'none');
+$('#tabbtnthree').click(function(){
+  $('#tabone').css('display', 'none');
+  $('#tabtwo').css('display', 'none');
+  $('#tabthree').css('display', 'block');
+  $('#tabfour').css('display', 'none');
 });
-$('.tabbtnfour').click(function(){
-  $('.tabone').css('display', 'none');
-  $('.tabtwo').css('display', 'none');
-  $('.tabthree').css('display', 'none');
-  $('.tabfour').css('display', 'block');
+$('#tabbtnfour').click(function(){
+  $('#tabone').css('display', 'none');
+  $('#tabtwo').css('display', 'none');
+  $('#tabthree').css('display', 'none');
+  $('#tabfour').css('display', 'block');
 });
