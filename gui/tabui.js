741e851a18fef0a0b72e376cfc8ffb233aa3fc02
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-07 20:27:25 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -5,20 +5,20 @@ $('.tabbtn1').click(function(){
   $('.tab4').css('display', 'none');
 });
 $('.tabbtn2').click(function(){
-  $('.tab1').css('display', 'block');
-  $('.tab2').css('display', 'none');
+  $('.tab1').css('display', 'none');
+  $('.tab2').css('display', 'block');
   $('.tab3').css('display', 'none');
   $('.tab4').css('display', 'none');
 });
 $('.tabbtn3').click(function(){
-  $('.tab1').css('display', 'block');
+  $('.tab1').css('display', 'none');
   $('.tab2').css('display', 'none');
-  $('.tab3').css('display', 'none');
+  $('.tab3').css('display', 'block');
   $('.tab4').css('display', 'none');
 });
 $('.tabbtn4').click(function(){
-  $('.tab1').css('display', 'block');
+  $('.tab1').css('display', 'none');
   $('.tab2').css('display', 'none');
   $('.tab3').css('display', 'none');
-  $('.tab4').css('display', 'none');
+  $('.tab4').css('display', 'block');
 });
