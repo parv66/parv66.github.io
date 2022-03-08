566681f7197b81a608d5d65321bb95432e794c33
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-08 13:35:51 +0530

    Update tabui.js

--- gui/tabui.js
+++ gui/tabui.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   $('#tabtwo').show();
+  $('#tabbtnone').addClass("tabbtnactive");
+  $('#tabbtntwo').removeClass("tabbtnactive");  
+  $('#tabbtnthree').removeClass("tabbtnactive");
+  $('#tabbtnfour').removeClass("tabbtnactive");  
   $('#tabtwo').hide();
   $('#tabthree').hide();
   $('#tabfour').hide();
   $( '#tabbtnone' ).on( 'click', function () {
     console.log('clicked one');
+    $('#tabbtnone').addClass("tabbtnactive");
+    $('#tabbtntwo').removeClass("tabbtnactive");
+    $('#tabbtnthree').removeClass("tabbtnactive");
+    $('#tabbtnfour').removeClass("tabbtnactive");
     $('#tabone').show();
     $('#tabtwo').hide();
     $('#tabthree').hide();
@@ -12,6 +20,10 @@ $(document).ready(function () {
   });
   $( '#tabbtntwo' ).on( 'click', function () {
     console.log('clicked 2');
+    $('#tabbtnone').removeClass("tabbtnactive");
+    $('#tabbtntwo').addClass("tabbtnactive");
+    $('#tabbtnthree').removeClass("tabbtnactive");
+    $('#tabbtnfour').removeClass("tabbtnactive");
     $('#tabone').hide();
     $('#tabtwo').show();
     $('#tabthree').hide();
@@ -19,6 +31,10 @@ $(document).ready(function () {
   });
   $( '#tabbtnthree' ).on( 'click', function () {
     console.log('clicked 3');
+    $('#tabbtnone').removeClass("tabbtnactive");
+    $('#tabbtntwo').removeClass("tabbtnactive");
+    $('#tabbtnthree').addClass("tabbtnactive");
+    $('#tabbtnfour').removeClass("tabbtnactive");
     $('#tabone').hide();
     $('#tabtwo').hide();
     $('#tabthree').show();
@@ -26,6 +42,10 @@ $(document).ready(function () {
   });
   $( '#tabbtnfour' ).on( 'click', function () {
     console.log('clicked 4')
+    $('#tabbtnone').removeClass("tabbtnactive");
+    $('#tabbtntwo').removeClass("tabbtnactive");
+    $('#tabbtnthree').removeClass("tabbtnactive");
+    $('#tabbtnfour').addClass("tabbtnactive");
     $('#tabone').hide();
     $('#tabtwo').hide();
     $('#tabthree').hide();
