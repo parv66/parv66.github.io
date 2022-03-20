b49173ac9ce8aad37877c7cd3058d15eb7eeedab
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-20 14:50:12 +0530

    Update jukebox.js

--- gui/jukebox.js
+++ gui/jukebox.js
@@ -255,7 +255,7 @@ $(function () {
 
   initPlayer();
 });
-$('body').onunload(function() {
+$('body').unload(function() {
    	var timestamp = audio.currentTime;
 	var songindex = currIndex;
 	document.cookie = "time= timestamp;sindex= songindex; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
