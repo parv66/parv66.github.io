d730a2357f33c77fcc06a40b7fa6d4381aae73a6
PARV66 <82944668+parv66@users.noreply.github.com>
2022-03-23 22:40:57 +0530

    Update contextmenu.js

--- gui/contextmenu.js
+++ gui/contextmenu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#contextmenu").hide();
         $(document).bind('contextmenu', function(event){
-		$("#contextmenu").css({"top": event.pageY + "px", "left": event.pageX + "px"}).show();
+		$("#contextmenu").css({"top": event.pageY-280 + "px", "left": event.pageX-2 + "px"}).show();
 		event.preventDefault();
 	});
 	$(document).bind('click', function(){
