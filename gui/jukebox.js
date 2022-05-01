582e2f38d395f73cf913596d3eedc837cf442ecf
PARV66 <82944668+parv66@users.noreply.github.com>
2022-05-01 22:31:48 +0530

    Update jukebox.js

--- gui/jukebox.js
+++ gui/jukebox.js
@@ -1,19 +1,16 @@
 /*! js-cookie v3.0.1 | MIT */
 !function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,o=e.Cookies=t();o.noConflict=function(){return e.Cookies=n,o}}())}(this,(function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)e[o]=n[o]}return e}return function t(n,o){function r(t,r,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n.write(r,t)+c}}return Object.create({set:r,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var t=document.cookie?document.cookie.split("; "):[],o={},r=0;r<t.length;r++){var i=t[r].split("="),c=i.slice(1).join("=");try{var u=decodeURIComponent(i[0]);if(o[u]=n.read(c,u),e===u)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){r(t,"",e({},n,{expires:-1}))},withAttributes:function(n){return t(this.converter,e({},this.attributes,n))},withConverter:function(n){return t(e({},this.converter,n),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(n)}})}({read:function(e){return'"'===e[0]&&(e=e.slice(1,-1)),e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}},{path:"/"})}));
-/*! jquery cookie */
-(function(factory){if(typeof define==='function'&&define.amd){define(['jquery'],factory);}else if(typeof exports==='object'){module.exports=factory(require('jquery'));}else{factory(jQuery);}}(function($){var pluses=/\+/g;function encode(s){return config.raw?s:encodeURIComponent(s);}function decode(s){return config.raw?s:decodeURIComponent(s);}function stringifyCookieValue(value){return encode(config.json?JSON.stringify(value):String(value));}function parseCookieValue(s){if(s.indexOf('"')===0){s=s.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,'\\');}try{s=decodeURIComponent(s.replace(pluses,' '));return config.json?JSON.parse(s):s;}catch(e){}}function read(s,converter){var value=config.raw?s:parseCookieValue(s);return $.isFunction(converter)?converter(value):value;}var config=$.cookie=function(key,value,options){if(arguments.length>1&&!$.isFunction(value)){options=$.extend({},config.defaults,options);if(typeof options.expires==='number'){var days=options.expires,t=options.expires=new Date();t.setMilliseconds(t.getMilliseconds()+days*864e+5);}return(document.cookie=[encode(key),'=',stringifyCookieValue(value),options.expires?'; expires='+options.expires.toUTCString():'',options.path?'; path='+options.path:'',options.domain?'; domain='+options.domain:'',options.secure?'; secure':''].join(''));}var result=key?undefined:{},cookies=document.cookie?document.cookie.split('; '):[],i=0,l=cookies.length;for(;i<l;i++){var parts=cookies[i].split('='),name=decode(parts.shift()),cookie=parts.join('=');if(key===name){result=read(cookie,value);break;}if(!key&&(cookie=read(cookie))!==undefined){result[name]=cookie;}}return result;};config.defaults={};$.removeCookie=function(key,options){$.cookie(key,'',$.extend({},options,{expires:-1}));return!$.cookie(key);};}));
-//eee
+
 $( document ).ready(function() {
-   	$("body").prepend("<div id='jukebox' class='jukehide'><div id='player'><div id='player-track' class='active'><div id='album-name'>Dawn</div><div id='track-name'>Skylike - Dawn</div><div id='track-time' class='active'><div id='current-time'>00:00</div><div id='track-length'>03:09</div></div><div id='s-area'><div id='ins-time' style='left: 0px; margin-left: 0px; display: none;'>00:00</div><div id='s-hover' style='width: 0px;'></div><div id='seek-bar' style='width: 0px;'></div></div></div><div id='player-content'><div id='album-art' class=''><img src='https://parv66.github.io/icon/record/record_otherside.png' class='active' id='_1'><img src='https://parv66.github.io/icon/record/record_pigstep.png' id='_2'><img src='https://parv66.github.io/icon/record/record_stal.png' id='_3'><img src='https://parv66.github.io/icon/record/record_mall.png' id='_4'><img src='https://parv66.github.io/icon/record/record_wait.png' id='_5'><div id='buffer-box'>Buffering ...</div></div><div id='player-controls'><div class='control'><div class='button' id='play-previous'><a>&lt;&lt;</a></div></div><div class='control'><div class='button' id='play-pause-button'><a>||</a></div></div><div class='control'><div class='button' id='play-next'><a>&gt;&gt;</a></div></div></div></div></div><div id='minimize'><button id='toggleview' class='button mcbtn'>!</button></div></div>");
+   	$("body").prepend("<div id='jukebox' class='jukehide'><div id='player'><div id='player-track' class='active'><div id='album-name'>Otherside</div><div id='track-name'>Lena Raine</div><div id='track-time' class='active'><div id='current-time'>00:00</div><div id='track-length'>03:09</div></div><div id='s-area'><div id='ins-time' style='left: 0px; margin-left: 0px; display: none;'>00:00</div><div id='s-hover' style='width: 0px;'></div><div id='seek-bar' style='width: 0px;'></div></div></div><div id='player-content'><div id='album-art' class=''><img src='https://parv66.github.io/icon/record/record_otherside.png' class='active' id='_1'><img src='https://parv66.github.io/icon/record/record_pigstep.png' id='_2'><img src='https://parv66.github.io/icon/record/record_stal.png' id='_3'><img src='https://parv66.github.io/icon/record/record_wait.png' id='_4'><img src='https://parv66.github.io/icon/record/record_mall.png' id='_5'><div id='buffer-box'>Buffering ...</div></div><div id='player-controls'><div class='control'><div class='button' id='play-previous'><a>&lt;&lt;</a></div></div><div class='control'><div class='button' id='play-pause-button'><a>||</a></div></div><div class='control'><div class='button' id='play-next'><a>&gt;&gt;</a></div></div></div></div></div><div id='minimize'><button id='toggleview' class='button mcbtn'>!</button></div></div>");
 	$('#toggleview').click(function(){
 		$('#jukebox').toggleClass("jukehide");
-	});
-	
+	});	
 });
+//js-cookie end//
+
 $(function () {
   var playerTrack = $("#player-track"),
-    bgArtwork = $("#bg-artwork"),
-    bgArtworkUrl,
     albumName = $("#album-name"),
     trackName = $("#track-name"),
     albumArt = $("#album-art"),
@@ -42,45 +39,70 @@ $(function () {
     buffInterval = null,
     tFlag = false,
     albums = [
-      "Lena raine",
-      "Lena raine",
-      "c418",
-      "c418",
-      "c418"
+      "Otherside",
+      "Pigstep",
+      "Stal",
+      "Mall",
+      "Wait"
     ],
     trackNames = [
-      "otherside",
-      "pigstep",
-      "stal",
-      "subwooferlullaby",
-      "mall"
+      "Lena Raine",
+      "Lena Raine",
+      "C148",
+      "C148",
+      "C148"
     ],
     albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
     trackUrl = [
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/gui/music/otherside.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/gui/music/pigstep.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/gui/music/stal.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/gui/music/subwooferlullaby.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/gui/music/mall.mp3"
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/otherside.mp3",
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/pigstep.mp3",
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/stal.mp3",
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/mall.mp3",
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/wait.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
-    playNextTrackButton = $("#play-next"),
-    currIndex = -1;
+    playNextTrackButton = $("#play-next")
+  
+//cookie check
+var exists = sessionStorage.getItem('exists');
 
+if (!exists) {
+	console.log("NEWBIE INCOMEING I REPEAt, NEWBIE INCOMMING");
+    	sessionStorage.setItem('exists', true);
+	Cookies.set('timestamp', 0, {expires: 7}, {path: '/jukebox'});
+	Cookies.set('track', -1, {expires: 7}, {path: '/jukebox'});
+	sessionStorage.setItem('timestamp', 0);
+	sessionStorage.setItem('track', -1);
+}
+if (exists) {
+	console.log("oh well look who it is...");
+	var currIndex = Cookies.get('track');
+};
+var loadstamp = (function() {
+    var loaded = false;
+    return function() {
+        if (!loaded) {
+            loaded = true;
+	    var currIndex = sessionStorage.getItem('track');
+            audio.currentTime = sessionStorage.getItem('timestamp');
+        }
+    };
+})();
+//cookie check end
+	
   function playPause() {
     setTimeout(function () {
       if (audio.paused) {
+	loadstamp();
         playerTrack.addClass("active");
         albumArt.addClass("active");
         checkBuffering();
-        i.attr("class", "fas fa-pause");
         audio.play();
       } else {
         playerTrack.removeClass("active");
         albumArt.removeClass("active");
         clearInterval(buffInterval);
         albumArt.removeClass("buffering");
-        i.attr("class", "fas fa-play");
         audio.pause();
       }
     }, 300);
@@ -110,7 +132,6 @@ $(function () {
 
     insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
   }
-
   function hideHover() {
     sHover.width(0);
     insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
@@ -125,7 +146,7 @@ $(function () {
   function updateCurrTime() {
     nTime = new Date();
     nTime = nTime.getTime();
-
+    console.log(currIndex);
     if (!tFlag) {
       tFlag = true;
       trackTime.addClass("active");
@@ -163,14 +184,13 @@ $(function () {
     seekBar.width(playProgress + "%");
 
     if (playProgress == 100) {
-      i.attr("class", "fa fa-play");
       seekBar.width(0);
       tProgress.text("00:00");
       albumArt.removeClass("buffering").removeClass("active");
       clearInterval(buffInterval);
     }
   }
-
+  
   function checkBuffering() {
     clearInterval(buffInterval);
     buffInterval = setInterval(function () {
@@ -185,7 +205,7 @@ $(function () {
   function selectTrack(flag) {
     if (flag == 0 || flag == 1) ++currIndex;
     else --currIndex;
-
+	  
     if (currIndex > -1 && currIndex < albumArtworks.length) {
       if (flag == 0) i.attr("class", "fa fa-play");
       else {
@@ -220,10 +240,6 @@ $(function () {
       trackName.text(currTrackName);
       albumArt.find("img.active").removeClass("active");
       $("#" + currArtwork).addClass("active");
-
-      bgArtworkUrl = $("#" + currArtwork).attr("src");
-
-      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
     } else {
       if (flag == 0 || flag == 1) --currIndex;
       else ++currIndex;
@@ -234,7 +250,8 @@ $(function () {
     audio = new Audio();
 
     selectTrack(0);
-
+    playPause();
+    $('#jukebox').toggleClass("jukehide");
     audio.loop = true;
 
     playPauseButton.on("click", playPause);
@@ -258,10 +275,12 @@ $(function () {
   }
 
   initPlayer();
-});
-$(window).on("beforeunload", function() { 
-   	var timestamp = audio.currentTime;
-	var songindex = currIndex;
-	Cookies.set('timestamp', audio.currentTime, {expires: 7}, {path: 'jukebox'})
-	Cookies.set('track', audio.currentTime, {expires: 7}, {path: 'jukebox'})
+	$(window).on("beforeunload", function() {
+		var songid = currIndex-1;
+   		var timestamp = audio.currentTime;
+		Cookies.set('timestamp', audio.currentTime, {expires: 7}, {path: '/jukebox'})
+		Cookies.set('track', songid, {expires: 7}, {path: '/jukebox'})
+		sessionStorage.setItem('timestamp', audio.currentTime);
+		sessionStorage.setItem('track', songid);
+    });
 });
