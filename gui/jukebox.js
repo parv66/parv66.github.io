58c56d5c1fd86dbb0d1262ede3cead78b2b35368
PARV66 <82944668+parv66@users.noreply.github.com>
2022-05-01 21:31:18 +0530

    Update jukebox.js

--- gui/jukebox.js
+++ gui/jukebox.js
@@ -1,13 +1,18 @@
-/*! js-cookie v3.0.1 | MIT */
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,o=e.Cookies=t();o.noConflict=function(){return e.Cookies=n,o}}())}(this,(function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)e[o]=n[o]}return e}return function t(n,o){function r(t,r,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n.write(r,t)+c}}return Object.create({set:r,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var t=document.cookie?document.cookie.split("; "):[],o={},r=0;r<t.length;r++){var i=t[r].split("="),c=i.slice(1).join("=");try{var u=decodeURIComponent(i[0]);if(o[u]=n.read(c,u),e===u)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){r(t,"",e({},n,{expires:-1}))},withAttributes:function(n){return t(this.converter,e({},this.attributes,n))},withConverter:function(n){return t(e({},this.converter,n),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(n)}})}({read:function(e){return'"'===e[0]&&(e=e.slice(1,-1)),e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}},{path:"/"})}));
-
 $( document ).ready(function() {
-   	$("body").prepend("<div id='jukebox' class='jukehide'><div id='player'><div id='player-track' class='active'><div id='album-name'>Otherside</div><div id='track-name'>Lena Raine</div><div id='track-time' class='active'><div id='current-time'>00:00</div><div id='track-length'>03:09</div></div><div id='s-area'><div id='ins-time' style='left: 0px; margin-left: 0px; display: none;'>00:00</div><div id='s-hover' style='width: 0px;'></div><div id='seek-bar' style='width: 0px;'></div></div></div><div id='player-content'><div id='album-art' class=''><img src='https://parv66.github.io/icon/record/record_otherside.png' class='active' id='_1'><img src='https://parv66.github.io/icon/record/record_pigstep.png' id='_2'><img src='https://parv66.github.io/icon/record/record_stal.png' id='_3'><img src='https://parv66.github.io/icon/record/record_wait.png' id='_4'><img src='https://parv66.github.io/icon/record/record_mall.png' id='_5'><div id='buffer-box'>Buffering ...</div></div><div id='player-controls'><div class='control'><div class='button' id='play-previous'><a>&lt;&lt;</a></div></div><div class='control'><div class='button' id='play-pause-button'><a>||</a></div></div><div class='control'><div class='button' id='play-next'><a>&gt;&gt;</a></div></div></div></div></div><div id='minimize'><button id='toggleview' class='button mcbtn'>!</button></div></div>");
+   	$("body").prepend("<div id='jukebox' class='jukehide'><div id='player'><div id='player-track' class='active'><div id='album-name'>Otherside</div><div id='track-name'>Lena Raine</div><div id='track-time' class='active'><div id='current-time'>00:00</div><div id='track-length'>03:09</div></div><div id='s-area'><div id='ins-time' style='left: 0px; margin-left: 0px; display: none;'>00:00</div><div id='s-hover' style='width: 0px;'></div><div id='seek-bar' style='width: 0px;'></div></div></div><div id='player-content'><div id='album-art' class=''><img src='https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg' class='active' id='_1'><img src='https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_2.jpg' id='_2'><img src='https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_3.jpg' id='_3'><img src='https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_4.jpg' id='_4'><img src='https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_5.jpg' id='_5'><div id='buffer-box'>Buffering ...</div></div><div id='player-controls'><div class='control'><div class='button' id='play-previous'><a>&lt;&lt;</a></div></div><div class='control'><div class='button' id='play-pause-button'><a>||</a></div></div><div class='control'><div class='button' id='play-next'><a>&gt;&gt;</a></div></div></div></div></div><div id='minimize'><button id='toggleview' class='button mcbtn'>!</button></div></div>");
 	$('#toggleview').click(function(){
 		$('#jukebox').toggleClass("jukehide");
 	});	
 });
-//js-cookie end//
+var loadstamp = (function() {
+    var loaded = false;
+    return function() {
+        if (!loaded) {
+            loaded = true;
+            audio.currentTime = Cookies.get('timestamp');
+        }
+    };
+})();
 
 $(function () {
   var playerTrack = $("#player-track"),
@@ -42,8 +47,8 @@ $(function () {
       "Otherside",
       "Pigstep",
       "Stal",
-      "Mall",
-      "Wait"
+      "Subwoofer Lullaby",
+      "Minecraft"
     ],
     trackNames = [
       "Lena Raine",
@@ -57,43 +62,19 @@ $(function () {
       "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/otherside.mp3",
       "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/pigstep.mp3",
       "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/stal.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/mall.mp3",
-      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/wait.mp3"
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/subwooferlullaby.mp3",
+      "https://raw.githubusercontent.com/parv66/parv66.github.io/master/gui/music/minecraft.mp3"
     ],
     playPreviousTrackButton = $("#play-previous"),
     playNextTrackButton = $("#play-next")
-  
-//cookie check
-var exists = sessionStorage.getItem('exists');
-
-if (!exists) {
-	console.log("NEWBIE INCOMEING I REPEAt, NEWBIE INCOMMING");
-    	sessionStorage.setItem('exists', true);
-	Cookies.set('timestamp', 0, {expires: 7}, {path: '/jukebox'});
-	Cookies.set('track', -1, {expires: 7}, {path: '/jukebox'});
-	sessionStorage.setItem('timestamp', 0);
-	sessionStorage.setItem('track', -1);
-}
-if (exists) {
-	console.log("oh well look who it is...");
-	var currIndex = Cookies.get('track');
-};
-var loadstamp = (function() {
-    var loaded = false;
-    return function() {
-        if (!loaded) {
-            loaded = true;
-	    var currIndex = sessionStorage.getItem('track');
-            audio.currentTime = sessionStorage.getItem('timestamp');
-        }
-    };
-})();
-//cookie check end
-	
+  if (Cookies.get('track')) {
+    var currIndex = Cookies.get('track');
+  }
+  else {var currIndex = -0;}
   function playPause() {
+    loadstamp();
     setTimeout(function () {
       if (audio.paused) {
-	loadstamp();
         playerTrack.addClass("active");
         albumArt.addClass("active");
         checkBuffering();
@@ -184,6 +165,7 @@ var loadstamp = (function() {
     seekBar.width(playProgress + "%");
 
     if (playProgress == 100) {
+      i.attr("class", "fa fa-play");
       seekBar.width(0);
       tProgress.text("00:00");
       albumArt.removeClass("buffering").removeClass("active");
@@ -250,8 +232,7 @@ var loadstamp = (function() {
     audio = new Audio();
 
     selectTrack(0);
-    playPause();
-    $('#jukebox').toggleClass("jukehide");
+
     audio.loop = true;
 
     playPauseButton.on("click", playPause);
@@ -276,11 +257,9 @@ var loadstamp = (function() {
 
   initPlayer();
 	$(window).on("beforeunload", function() {
-		var songid = currIndex-1;
-   		var timestamp = audio.currentTime;
-		Cookies.set('timestamp', audio.currentTime, {expires: 7}, {path: '/jukebox'})
-		Cookies.set('track', songid, {expires: 7}, {path: '/jukebox'})
-		sessionStorage.setItem('timestamp', audio.currentTime);
-		sessionStorage.setItem('track', songid);
+	var songid = currIndex - 1;
+   	var timestamp = audio.currentTime;
+	Cookies.set('timestamp', audio.currentTime, {expires: 7}, {path: '/jukebox'})
+	Cookies.set('track', songid, {expires: 7}, {path: '/jukebox'})
     });
 });
